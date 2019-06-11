8b93dcb59eb36716c9e9f180a5a03d2b55165551
Rendhy Pratama Putra <rendhypratama880@gmail.com>
2019-06-11 21:21:28 +0700

    BIKEA

--- rental-api/application/models/m_mobil.php
+++ rental-api/application/models/m_mobil.php
@@ -7,20 +7,20 @@ class m_mobil extends CI_Model
 {
 	private $table_name="tb_mobil";
 	private $table_gallery="tb_gallery_mobil";
-	private $primary="ID_MOBIL";
-	private $plat="PLAT_NO_MOBIL"; 
+	private $primary="PLAT_NO_MOBIL";
+	private $nama="NAMA_MOBIL"; 
 
 	function get_all(){
 		$this->db->select($this->table_name.'.*,GROUP_CONCAT(IMAGE) AS IMAGE');
-		$this->db->join($this->table_gallery." tb_gal",$this->table_name.'.ID_MOBIL'.'=tb_gal.ID_MOBIL','left');
-		$this->db->group_by($this->plat);
+		$this->db->join($this->table_gallery." tb_gal",$this->table_name.'.PLAT_NO_MOBIL'.'=tb_gal.PLAT_NO_MOBIL','left');
+		$this->db->group_by($this->nama);
 		return $this->db->get($this->table_name)->result();
 	}
 
 	function get_by_id($id){
 		$this->db->select($this->table_name.'.*,GROUP_CONCAT(IMAGE) AS IMAGE');
-		$this->db->join($this->table_gallery." tb_gal",$this->table_name.'.ID_MOBIL'.'=tb_gal.ID_MOBIL','left');
-		$this->db->group_by($this->plat);
+		$this->db->join($this->table_gallery." tb_gal",$this->table_name.'.PLAT_NO_MOBIL'.'=tb_gal.PLAT_NO_MOBIL','left');
+		$this->db->group_by($this->nama);
 		$this->db->where($this->table_name.'.'.$this->primary,$id);
 		return $this->db->get($this->table_name)->row();
 	}
@@ -30,7 +30,7 @@ class m_mobil extends CI_Model
 		$id=$this->db->insert_id();
 
 		if ($mobil_photo!=null&&$id) {
-			$mobil_photo["ID_MOBIL"]=$id;
+			$mobil_photo["PLAT_NO_MOBIL"]=$id;
 			$insert=$this->db->insert($this->table_gallery,$mobil_photo);
 		}
 	
@@ -42,7 +42,7 @@ class m_mobil extends CI_Model
 		$update=$this->db->update($this->table_name,$data);
 
 		if ($mobil_photo!=null&&$update) {
-			$mobil_photo["ID_MOBIL"]=$id;
+			$mobil_photo["PLAT_NO_MOBIL"]=$id;
 			$this->db->where($this->primary,$id);
 			$update=$this->db->update($this->table_gallery,$mobil_photo);
 		}
